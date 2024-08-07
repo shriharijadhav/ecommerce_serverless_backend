0abebca0f2b75781b63b6f37ecae72e64c262636
shriharijadhav <mr.shrihari212@gmail.com>
2024-08-07 10:02:40 +0530

    Demo 2

--- api/hello.js
+++ api/hello.js
@@ -1,10 +1,6 @@
-
 export default function handler(request, response) {
-  const dotenv = require('dotenv')
-dotenv.config()
-
   return response.json({
-    "message": "Hello world",
-    "url":process.env.DATABASE_URL
-  })
+    message: "Hello world",
+    url: process.env.DATABASE_URL,
+  });
 }
