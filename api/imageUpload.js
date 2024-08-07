4c72889d2c6cc1f4623a098932bd48b942e400f6
shriharijadhav <mr.shrihari212@gmail.com>
2024-08-07 20:42:42 +0530

    Demo 2

--- api/imageUpload.js
+++ api/imageUpload.js
@@ -12,6 +12,11 @@ cloudinary.config({
 });
 
 export default async function handler(req, res) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    
   if (req.method === 'OPTIONS') {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
