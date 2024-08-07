efda4fb79cd671578ebbd9060a07dc8551eeeb52
shriharijadhav <mr.shrihari212@gmail.com>
2024-08-07 14:39:26 +0530

    Demo 2

--- api/updateTodo.js
+++ api/updateTodo.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
 
     // Set CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PATCH, OPTIONS'); // Allow specific methods
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Allow all methods
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow specific headers
 
     try {
