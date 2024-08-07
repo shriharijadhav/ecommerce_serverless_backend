0d7108f2405e36c7ee940a7198aed0f21fcfbd79
shriharijadhav <mr.shrihari212@gmail.com>
2024-08-07 14:45:44 +0530

    Demo 2

--- api/updateTodo.js
+++ api/updateTodo.js
@@ -2,36 +2,40 @@ const todoModel = require('../model/todoModel');
 const dbConnect = require('../config/dbConnect');
 
 export default async function handler(req, res) {
-    await dbConnect();
-
     // Set CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Allow all methods
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow specific headers
 
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
+    await dbConnect();
+
     try {
         const updatedTodo = req.body;
         const id = updatedTodo._id;
         
-        const dataToUpdate = {todoName:updatedTodo.todoName,assigneeName:updatedTodo.assigneeName,priority:updatedTodo.priority}
-        const response = await todoModel.findByIdAndUpdate(id,dataToUpdate,{new:true});
+        const dataToUpdate = { todoName: updatedTodo.todoName, assigneeName: updatedTodo.assigneeName, priority: updatedTodo.priority };
+        const response = await todoModel.findByIdAndUpdate(id, dataToUpdate, { new: true });
         
-        if(response){
+        if (response) {
             return res.status(200).json({
-                message:"Todo updated successfully",
-                todoUpdated:true,
-            })
+                message: "Todo updated successfully",
+                todoUpdated: true,
+            });
         }
 
-        return res.status(200).json({
-            message:"Failed to update todo",
-            todoUpdated:false,
-        })
-
+        return res.status(400).json({
+            message: "Failed to update todo",
+            todoUpdated: false,
+        });
 
     } catch (error) {
         return res.status(500).json({
-            error: 'Failed to update todos'
+            error: 'Failed to update todos',
         });
     }
 }
