aaabd531dd473127c514c50968632184ff724de3
shriharijadhav <mr.shrihari212@gmail.com>
2024-08-12 18:55:39 +0530

    place order added

--- api/user/login.js
+++ api/user/login.js
@@ -65,7 +65,7 @@ export default async function login(req, res) {
         // fetch all the data of user from multiple collections and send in response
         const userAddresses = await addressModel.find({ user: userFromDB._id})
         const userCart = await cartModel.findOne({ user: userFromDB._id})
-        const ordersPlaced = await placedOrderModel.findOne({ user: userFromDB._id}).populate('products').exec()
+        const ordersPlaced = await placedOrderModel.findOne({ user: userFromDB._id}).populate('user').exec()
         const userProfileInfo = {firstName:userFromDB.firstName, lastName:userFromDB.lastName,email:userFromDB.email,contact:userFromDB.contact,userId:userFromDB._id}
         
         const userData = {
