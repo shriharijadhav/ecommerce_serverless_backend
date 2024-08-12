e828d3a4b9cb25b7a792acd1a948291be96775cb
shriharijadhav <mr.shrihari212@gmail.com>
2024-08-12 19:36:31 +0530

    place order added

--- api/order/placeOrder.js
+++ api/order/placeOrder.js
@@ -84,7 +84,7 @@ export default async function handler(req, res) {
             { $set: { allProductsInCart: [] } }
         );
 
-        const ordersPlaced = await placedOrderModel.findOne({ user: userFromDB._id }).populate('products').exec();
+        const ordersPlaced = await placedOrderModel.findOne({ user: userId }).populate('products').exec();
 
         return res.status(201).json({
             message: 'Order placed successfully',
