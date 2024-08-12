e13dc9e69181e2453f2322baec139258f407d7e3
shriharijadhav <mr.shrihari212@gmail.com>
2024-08-12 18:05:24 +0530

    place order added

--- model/placedOrderModel.js
+++ model/placedOrderModel.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const placedOrderSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true }],
-    address_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Address', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'product', required: true }],
+    address_id: { type: mongoose.Schema.Types.ObjectId, ref: 'address', required: true },
     contact_number: { type: String, required: true },
     orderPlacedAt: { type: Date, default: Date.now }, // This field will store the date and time of order placement
 });
